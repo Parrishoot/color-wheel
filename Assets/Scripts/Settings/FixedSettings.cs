bd8581f46d0228b10f6c0f6d01bc8243ae23f74c
Parrishoot <mparrish@u.rochester.edu>
2024-05-13 19:56:26 -0400

    Making UI Texture a lot smaller

---
+++ Assets/Scripts/Settings/FixedSettings.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class FixedSettings : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        QualitySettings.vSyncCount = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
